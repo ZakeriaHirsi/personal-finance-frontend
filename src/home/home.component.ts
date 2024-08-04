bfd7753c7a01b78b1c8ee9826f164216425072bf
Zakeria Hirsi <officialhirsi@gmail.com>
2024-08-04 01:27:40 +0100

    Deleting template code

---
+++ src/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    standalone: true,
+    imports: [],
+    templateUrl: './home.component.html',
+    //styleUrl: './app.component.scss'
+  })
+  export class Home {
+    title = 'big boss';
+  }
+  
