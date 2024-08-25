018b8fb199914e1236af9e5688dbb3cf034f10fc
Zakeria Hirsi <officialhirsi@gmail.com>
2024-08-25 03:09:36 +0100

    Adding stats

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { AuthComponent } from '../pages/login/auth/auth.component';
 import { AuthRegisterComponent } from '../pages/login/auth-register/auth-register.component';
 import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 export const routes: Routes = [
-  { path: '', component: Home },
   { path: 'login', component: AuthComponent },
   { path: 'register', component: AuthRegisterComponent},
   { path: 'dashboard', component: DashboardComponent},
+  { path: '', component: Home },
 ];
